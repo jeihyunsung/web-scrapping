2864d84d014449251a9a9160a15a2f8317f1a08f
jeihyunsung <hyunsungjei@naver.com>
2021-08-29 19:55:04 +0900

    Database format modified / data analyze

--- db.py
+++ db.py
@@ -3,38 +3,31 @@ import os.path
 
 class Database:
     def __init__(self):
-        if os.path.isfile('Database.csv'):
-            self.openDB()
-        else:
-            self.db = pd.DataFrame({})
+        # if os.path.isfile('./Database.csv'):
+        #     self.openDB()
+        # else:
+        self.db = pd.DataFrame({},columns = ['page', 'idx', 'year', 'method', 'area', 'st_price', 'final_price', 'auction_year', 'auction_month', 'auction_date'])
 
     def openDB(self):
-        self.db = pd.read_csv('Database.csv', index_col=0)
+        self.db = pd.read_csv('Database.csv')
 
     def saveDB(self):
         self.db.to_csv("Database.csv")
 
     def getID(self, id):
-        if id in self.db.keys():
-            str_data = self.db[id]
-        else:
-            str_data = ['','']
-        
-        return str_data[0], str_data[1]
+        str_data = self.db.loc[id]        
+        return str_data
     
     def deleteID(self, id):
-        if id in self.db:
-            self.db.drop(id, axis=1, inplace=True)
-            self.saveDB()
-        else:
-            return
+        self.db.drop(id, axis=0, inplace=True)
+        self.saveDB()
     
     def saveID(self, id, data):
-        self.db[id] = data
+        self.db.loc[id] = data
         self.saveDB()
 
     def updateID(self, id, data):
-        self.db[id] = data
+        self.db.loc[id] = data
         self.saveDB()
 
 
