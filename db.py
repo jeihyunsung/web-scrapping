73fa2ef9f4d8fec0e21e0beec4c1b6b3dcd2264a
jeihyunsung <hyunsungjei@naver.com>
2021-09-28 20:06:31 +0900

    Exclude data extraction script
    Exclude data extraction script

--- db.py
+++ db.py
@@ -1,12 +1,18 @@
 import pandas as pd
-import os.path
+import os
+from datetime import datetime
 
 class Database:
     def __init__(self):
-        # if os.path.isfile('./Database.csv'):
-        #     self.openDB()
-        # else:
+        if os.path.isfile('./Database.csv'):
+            self.backupDB()
         self.db = pd.DataFrame({},columns = ['page', 'idx', 'year', 'method', 'area', 'st_price', 'final_price', 'auction_year', 'auction_month', 'auction_date'])
+
+    def backupDB(self):
+        now = datetime.now()
+        date_time = now.strftime("%Y%m%d%H%M%S")
+        print(os.path)
+        os.rename('./Database.csv', f'./Database_backup_{date_time}.csv')
 
     def openDB(self):
         self.db = pd.read_csv('Database.csv')
@@ -15,22 +21,19 @@ class Database:
         self.db.to_csv("Database.csv")
 
     def getID(self, id):
-        str_data = self.db.loc[id]        
-        return str_data
+        str_data = self.db.loc[id]
     
     def deleteID(self, id):
         self.db.drop(id, axis=0, inplace=True)
-        self.saveDB()
     
     def saveID(self, id, data):
         self.db.loc[id] = data
-        self.saveDB()
 
     def updateID(self, id, data):
         self.db.loc[id] = data
-        self.saveDB()
 
 
-
+if __name__ == "__main__":
+    tmp_class = Database()
     
 
